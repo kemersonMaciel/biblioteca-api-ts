4dcc4fae305a82d6cc3dde9b31c2ca4b489782cd
luiz oliveira <oliveiraluizcarlosb@gmail.com>
2024-02-24 00:16:24 -0400

    delete pasta

--- src/modules/participante/dto/update-participante.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/swagger";
-import { CreateParticipanteDto } from "./create-participante.dto";
-
-export class UpdateParticipanteDto extends PartialType(CreateParticipanteDto) {}
